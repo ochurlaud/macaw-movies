8f39675fbbfa1a1e3844214310bf2d3528201bac
Olivier CHURLAUD <olivier@churlaud.com>
2014-08-07 14:47:16 +0200

    suppression of a QFileInfoList test

--- src/main.cpp
+++ src/main.cpp
@@ -30,13 +30,6 @@ int main(int argc, char *argv[])
     Application l_app(argc, argv);
     MainWindow l_mainWindow;
 
-    QDir l_directory("c:/Users/Olivier/Downloads/Movies");
-    QFileInfoList l_list = l_directory.entryInfoList();
-    for (int i = 0; i < l_list.size(); ++i) {
-        QFileInfo l_fileInfo = l_list.at(i);
-        qDebug() << QString(l_fileInfo.fileName());
-    }
-
     l_mainWindow.show();
 
     return l_app.exec();
