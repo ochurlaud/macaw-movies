136d3aef68b2adb6e873797dcf0b6676b9a86ce1
Olivier CHURLAUD <olivier@churlaud.com>
2015-05-09 15:35:41 +0200

    increment version

--- src/include_var.h
+++ src/include_var.h
@@ -1,7 +1,7 @@
 // software version major.minor.patch (eg: 0.1.0)
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "1"
-#define VERSION_PATCH "0"
+#define VERSION_PATCH "1"
 #define APP_VERSION VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH
 
 #define DB_VERSION 1    //database version, must be updated if the database model have changed
