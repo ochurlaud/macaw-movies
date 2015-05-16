59515cd8693b1d4b22c177963956324d4d32f9a6
Olivier CHURLAUD <olivier@churlaud.com>
2015-05-17 01:56:33 +0200

    translations files, cf #115

---
+++ translate/macaw_de.ts
@@ -0,0 +1,363 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../src/Application.cpp" line="74"/>
+        <source>The tag &lt;b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Application.cpp" line="75"/>
+        <source>Do you want to delete this tag?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Application.cpp" line="98"/>
+        <source>The person &lt;b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Application.cpp" line="99"/>
+        <source>Do you want to delete it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FetchMetadataDialog</name>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="20"/>
+        <source>Title to search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="35"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="42"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="51"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FetchMetadata/FetchMetadataDialog.ui" line="60"/>
+        <source>Don&apos;t ask me</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LeftPannel</name>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="66"/>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="70"/>
+        <source>Tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="75"/>
+        <source>Directors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="80"/>
+        <source>Producers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="85"/>
+        <source>Actors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="133"/>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="136"/>
+        <source>Edit Metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/LeftPannel.ui" line="139"/>
+        <source>M</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.ui" line="33"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="101"/>
+        <source>To Watch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="116"/>
+        <source>Search here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="168"/>
+        <source>Settin&amp;gs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="174"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="184"/>
+        <source>&amp;Edit Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="189"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="299"/>
+        <source>About </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="300"/>
+        <source>&lt;h1&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MetadataPannel</name>
+    <message>
+        <location filename="../src/MainWindowWidgets/MetadataPannel.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MetadataPannel.cpp" line="28"/>
+        <source>&lt;i&gt;Directed by&lt;/i&gt;&lt;br /&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MetadataPannel.cpp" line="29"/>
+        <source>&lt;i&gt;Produced by&lt;/i&gt;&lt;br /&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MetadataPannel.cpp" line="30"/>
+        <source>&lt;i&gt;With&lt;/i&gt;&lt;br /&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MovieDialog</name>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="17"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="28"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="38"/>
+        <source>Original Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="48"/>
+        <source>Release Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="55"/>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="65"/>
+        <source>Synopsis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="133"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="152"/>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="184"/>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="224"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="194"/>
+        <source>Actors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="201"/>
+        <source>Director(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="208"/>
+        <source>Producer(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="242"/>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="257"/>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="270"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="289"/>
+        <source>Add a tag here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="298"/>
+        <source>New tag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="310"/>
+        <source>add tag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/MovieDialog.ui" line="321"/>
+        <source>Tag list, select the tags for this movie.  
+Multi-selection with &lt;Ctrl&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MoviesPannel</name>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="90"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="93"/>
+        <source>Del</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="98"/>
+        <source>addToPlaylist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="101"/>
+        <source>actionAddToPLaylist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="106"/>
+        <source>Edit Metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindowWidgets/MoviesPannel.ui" line="109"/>
+        <source>E</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PeopleDialog</name>
+    <message>
+        <location filename="../src/Dialogs/PeopleDialog.ui" line="17"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/PeopleDialog.ui" line="27"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/PeopleDialog.ui" line="54"/>
+        <source>Birthday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/PeopleDialog.ui" line="81"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/PeopleDialog.ui" line="103"/>
+        <source>Biography</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="17"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="36"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="43"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="50"/>
+        <source>Recheck</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="76"/>
+        <source>Folder path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.ui" line="86"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.cpp" line="55"/>
+        <source>Open Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.cpp" line="121"/>
+        <source>Choose an existant path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/Dialogs/SettingsWindow.cpp" line="123"/>
+        <source>This path is already known</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
