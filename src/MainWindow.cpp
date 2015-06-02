ab7c2045aaf795b40ecc91368354df422048753c
Jani Rutanen <jani.m.rutanen@gmail.com>
2015-06-02 20:03:31 +0200

    .m4v suffix is not included in the authorizedSuffixList #120
    ".m4v" added to the authorizedSuffixList

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -213,7 +213,8 @@ void MainWindow::addNewMovies()
                                    << "mp4"
                                    << "mpg"
                                    << "flv"
-                                   << "mov";
+                                   << "mov"
+                                   << "m4v";
             if (l_authorizedSuffixList.contains(l_fileSuffix, Qt::CaseInsensitive)) {
                 Macaw::DEBUG("[MainWindow.updateApp()] Suffix accepted");
                 bool l_movieExists = databaseManager->existMovie(l_filePath);
@@ -257,6 +258,25 @@ void MainWindow::fillMetadataPannel(const Movie &movie)
     m_metadataPannel->fill(movie);
 
     Macaw::DEBUG_OUT("[MainWindow] Exit fillMetadataPannel");
+}
+/**
+ * @brief Slot triggered when the metadata for a file is fetched.
+ *
+ * @author Jani RUTANEN <jani.m.rutanen@gmail.com>
+ *
+ * @param event
+ */
+void MainWindow::updateFetchStatus(int done, int todo)
+{
+    Macaw::DEBUG_OUT("[MainWindow] updateFetchStatus()");
+    if(done < todo)
+    {
+        this->statusBar()->showMessage(QString("Metadata for %1 / %2 Files Fetched").arg(done).arg(todo+done));
+    }
+    else
+    {
+        this->statusBar()->showMessage(QString("Fetching Metadata Ready. %1 / %2 Files Fetched").arg(done).arg(todo+done), 5000);
+    }
 }
 
 /**
