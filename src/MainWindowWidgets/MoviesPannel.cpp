1126d685ddaef453be511ae4898e149ef7c2f9b8
Jani Rutanen <jani.m.rutanen@gmail.com>
2015-06-04 13:02:30 +0200

    Added check for user selected media player.

--- src/MainWindowWidgets/MoviesPannel.cpp
+++ src/MainWindowWidgets/MoviesPannel.cpp
@@ -239,24 +239,25 @@ void MoviesPannel::on_tableWidget_itemDoubleClicked(QTableWidgetItem *item)
 
     Macaw::DEBUG("[MoviesPannel.startMovie()] Opened movie: " + l_movie.filePath());
 
-//    QDesktopServices::openUrl(QUrl("file://" + l_movie.filePath(), QUrl::TolerantMode));
 
-    QString l_executeMediaPlayerPath;
-#ifdef Q_OS_LINUX
-//    l_executeMediaPlayerPath(QString("source %1").arg(databaseManager->getMediaPlayerPath()));
-#endif
+    if (!databaseManager->getMediaPlayerPath().compare("")) {
+        QString l_executeMediaPlayerPath;
+        #ifdef Q_OS_LINUX
+            l_executeMediaPlayerPath(QString("source \"%1\"").arg(databaseManager->getMediaPlayerPath()));
+        #endif
+        #ifdef Q_OS_WIN
+            l_executeMediaPlayerPath = QString("\"%1\"").arg(databaseManager->getMediaPlayerPath());
+        #endif
+        #ifdef Q_OS_OSX
+            l_executeMediaPlayerPath = QString("open %1").arg(databaseManager->getMediaPlayerPath());
+        #endif
 
-#ifdef Q_OS_WIN
-  //  l_executeMediaPlayerPath = QString("%1").arg(databaseManager->getMediaPlayerPath());
-#endif
-
-#ifdef Q_OS_OSX
-    l_executeMediaPlayerPath = QString("open %1").arg(databaseManager->getMediaPlayerPath());
-#endif
-
-    QProcess::startDetached(QString("%1 \"%2\"")
-                                   .arg(l_executeMediaPlayerPath)
-                                   .arg(l_movie.filePath()));
+        QProcess::startDetached(QString("%1 \"%2\"")
+                                       .arg(l_executeMediaPlayerPath)
+                                       .arg(l_movie.filePath()));
+    } else {
+        QDesktopServices::openUrl(QUrl("file://" + l_movie.filePath(), QUrl::TolerantMode));
+    }
 }
 
 /**
@@ -673,4 +674,3 @@ bool MoviesPannel::permanentlyDeleteFile(QFile * movieFileToDelete) {
         }
         return true;
 }
-
