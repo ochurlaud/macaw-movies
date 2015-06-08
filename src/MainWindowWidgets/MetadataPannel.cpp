c9cd50bb30c11e2393631f9394e227f2b199de2f
Olivier CHURLAUD <olivier@churlaud.com>
2015-06-09 00:17:24 +0200

    enhance metadatapannel

--- src/MainWindowWidgets/MetadataPannel.cpp
+++ src/MainWindowWidgets/MetadataPannel.cpp
@@ -32,11 +32,21 @@ void MetadataPannel::fill(const Movie &movie)
     } else {
         this->show();
 
-        QString l_title = "<html>"+m_movie.title()+"<br />";
-        QString l_originalTitle = "<i>"+m_movie.originalTitle()+"</i></br /><br />";
-        QString l_directors = tr("<i>Directed by</i><br />");
-        QString l_producers = tr("<i>Produced by</i><br />");
-        QString l_actors = tr("<i>With</i><br />");
+        QString l_title = "<b>"+m_movie.title()+"</b>"+"<br />";
+        QString l_directors, l_producers, l_actors, l_originalTitle;
+        if (m_movie.originalTitle() != m_movie.title())
+        {
+            l_originalTitle = "<i>"+m_movie.originalTitle()+"</i><br />";
+        }
+        if (!m_movie.peopleList(People::Director).isEmpty()) {
+            l_directors = tr("<i>Directed by</i><br />");
+        }
+        if (!m_movie.peopleList(People::Producer).isEmpty()) {
+            l_producers = tr("<i>Produced by</i><br />");
+        }
+        if (!m_movie.peopleList(People::Actor).isEmpty()) {
+            l_actors = tr("<i>With</i><br />");
+        }
 
         foreach (People l_people, m_movie.peopleList()) {
             switch (l_people.type())
@@ -55,20 +65,26 @@ void MetadataPannel::fill(const Movie &movie)
         // Need to remove ", " = 2 chars
         if (l_directors.right(2) == ", ") {
             l_directors = l_directors.remove(l_directors.size()-2, 2);
-
+            l_directors += "<br /><br />";
         }
-        l_directors += "<br /><br />";
+
         if (l_producers.right(2) == ", ") {
             l_producers = l_producers.remove(l_producers.size()-2,2);
+            l_producers += "<br /><br />";
         }
-        l_producers += "<br /><br />";
+
         if (l_actors.right(2) == ", ") {
             l_actors = l_actors.remove(l_actors.size()-2,2);
+            l_actors += "<br /><br />";
         }
-        l_actors += "<br /><br />";
-        l_actors += "</html>";
 
-        m_ui->informationsLabel->setText(l_title+l_originalTitle + l_directors +l_producers + l_actors);
+
+        m_ui->informationsLabel->setText(l_title
+                                         + l_originalTitle
+                                         + "<br />"
+                                         + l_directors
+                                         + l_producers
+                                         + l_actors);
         m_ui->plotLabel->setText(m_movie.synopsis());
         this->setPoster();
     }
@@ -89,7 +105,7 @@ void MetadataPannel::setPoster()
                                + m_movie.posterPath();
         l_poster.load(l_posterPath);
 
-        QSize l_size(this->width()-30, this->height()/2.5);
+        QSize l_size(this->width()*0.9, this->height()/2.5);
         l_poster = l_poster.scaled(l_size, Qt::KeepAspectRatio);
     }
     m_ui->posterLabel->setPixmap(l_poster);
