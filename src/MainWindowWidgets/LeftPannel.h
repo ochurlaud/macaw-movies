eb3d1ebceb1a4c1754221f545dbb2a62b3a967af
Olivier CHURLAUD <olivier@churlaud.com>
2015-06-09 18:45:15 +0200

    add missing license

--- src/MainWindowWidgets/LeftPannel.h
+++ src/MainWindowWidgets/LeftPannel.h
@@ -1,3 +1,22 @@
+/* Copyright (C) 2014 Macaw-Movies
+ * (Olivier CHURLAUD, Sébastien TOUZÉ)
+ *
+ * This file is part of Macaw-Movies.
+ *
+ * Macaw-Movies is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Macaw-Movies is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Macaw-Movies.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef LEFTPANNEL_H
 #define LEFTPANNEL_H
 
